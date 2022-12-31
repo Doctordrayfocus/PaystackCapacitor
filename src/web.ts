f4bf60d3e3472560ddbcb3e76117700eb2533463
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-12-31 21:38:21 +0100

    fix: android build error

--- src/web.ts
+++ src/web.ts
@@ -2,26 +2,53 @@ import { WebPlugin } from '@capacitor/core';
 import { PaystackCapacitorPlugin } from './definitions';
 
 export class PaystackCapacitorWeb extends WebPlugin implements PaystackCapacitorPlugin {
-  constructor() {
-    super({
-      name: 'PaystackCapacitor',
-      platforms: ['web']
-    });
-  }
-  initialize(publicKey: string): Promise<{ initialized: boolean; }> {
-    throw new Error("Method not implemented.");
-  }
-  validateCard(cardNumber: string, expiryMonth: string, expiryYear: string, cvv: string): Promise<{ is_valid: boolean; }> {
-    throw new Error("Method not implemented.");
-  }
-  chargeCard(): Promise<{ reference: import("./definitions").Transaction; }> {
-    throw new Error("Method not implemented.");
-  }
+	constructor() {
+		super({
+			name: 'PaystackCapacitor',
+			platforms: ['web']
+		});
+	}
+	initialize(payload: { publicKey: string }): Promise<{ initialized: boolean; }> {
+		throw new Error("Method not implemented.");
+	}
+	addCard(payload: { cardNumber: string, expiryMonth: string, expiryYear: string, cvv: string }): Promise<{ is_valid: boolean; }> {
+		throw new Error("Method not implemented.");
+	}
+	chargeCard(): Promise<{ reference: string; }> {
+		throw new Error("Method not implemented.");
+	}
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
-  }
+	validateCard(): Promise<{ is_valid: boolean }> {
+		throw new Error("Method not implemented.");
+	}
+
+	addChargeParameters(parameters: { [key: string]: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	getCardType(): Promise<{ card_type: string }> {
+		throw new Error("Method not implemented.");
+	}
+	putChargeMetadata(metadata: { [key: string]: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	putChargeCustomFields(customFields: { [key: string]: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	setChargeEmail(payload: { email: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	setChargeAmount(payload: { amount: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+	setAccessCode(payload: { accessCode: string }): Promise<any> {
+		throw new Error("Method not implemented.");
+	}
+
+
+	async echo(options: { value: string }): Promise<{ value: string }> {
+		console.log('ECHO', options);
+		return options;
+	}
 }
 
 const PaystackCapacitor = new PaystackCapacitorWeb();
